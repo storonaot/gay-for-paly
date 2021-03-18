47a06db97ef06b0bcc915c74e94ab78312b1d0a4
Dmitriy <34fayfay@gmail.com>
2021-03-18 20:11:34 +0300

    Друзья

--- src/panels/Friends/index.js
+++ src/panels/Friends/index.js
@@ -1,15 +1,52 @@
 import React from 'react'
-import { Panel } from '@vkontakte/vkui'
+import { Avatar, Group, Header, Panel, SimpleCell } from '@vkontakte/vkui'
 
 import { PANELS } from '../../constants'
 
 import PanelHeader from '../../common/PanelHeader'
+import { useState } from 'react/cjs/react.production.min'
 
 const Friends = ({ id, title }) => {
+  let friends = [
+    {
+      id: 1,
+      first_name: 'Женя',
+      last_name: 'Соколов',
+      avatar: 'https://yt3.ggpht.com/a/AATXAJwjNes0pvUyXrwN2QvtNvpu0m0Z0HJ1bAC5Ng=s900-c-k-c0xffffffff-no-rj-mo',
+      accounts: ['Steam', 'Battle.net'],
+    },
+    {
+      id: 2,
+      first_name: 'Маша',
+      last_name: 'Соловьева',
+      avatar: 'https://yt3.ggpht.com/a/AATXAJwjNes0pvUyXrwN2QvtNvpu0m0Z0HJ1bAC5Ng=s900-c-k-c0xffffffff-no-rj-mo',
+      accounts: ['Steam'],
+    },
+    {
+      id: 3,
+      first_name: 'Геннадий',
+      last_name: 'Шпак',
+      avatar: 'https://yt3.ggpht.com/a/AATXAJwjNes0pvUyXrwN2QvtNvpu0m0Z0HJ1bAC5Ng=s900-c-k-c0xffffffff-no-rj-mo',
+      accounts: ['Battle.net'],
+    },
+  ]
+
   return (
     <Panel id={id}>
-      <PanelHeader title={title} goBack={PANELS.profile} />
-      <h1>Friends</h1>
+      <PanelHeader title={title} />
+      <Group>
+        <Header>Друзья</Header>
+        {
+          friends.map(friend => {
+            return (
+              <SimpleCell description={friend.accounts.join(', ')}
+                          before={<Avatar src={friend.avatar} />}>
+                {friend.first_name} {friend.last_name}
+              </SimpleCell>
+            )
+          })
+        }
+      </Group>
     </Panel>
   )
 }
