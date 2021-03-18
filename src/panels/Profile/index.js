bec09322b01765039bb305d6fc48dc0cd787fdb5
Dmitriy <34fayfay@gmail.com>
2021-03-18 18:31:47 +0300

    Верстка профиля

--- src/panels/Profile/index.js
+++ src/panels/Profile/index.js
@@ -3,38 +3,48 @@ import {
   Panel,
   PanelHeader,
   Group,
-  Cell,
   Header,
   Avatar,
   SimpleCell,
-  MiniInfoCell,
-  ModalRoot,
   FormItem,
-  Input,
-  ModalPage,
-  ModalPageHeader, FormLayout, Button, Spacing,
+  Input, FormLayout, Button, Spacing, RichCell, Title, IconButton, Caption,
 } from '@vkontakte/vkui'
 
-import Icon20ErrorCircleOutline from '@vkontakte/icons/dist/20/error_circle_outline'
+import Icon24UnfavoriteOutline from '@vkontakte/icons/dist/24/unfavorite_outline'
+import { Icon12User } from '@vkontakte/icons'
+import Dropdown from '../../common/Dropdown'
 
 const Profile = ({ id, user, title }) => {
-
-  const ABOUT_MODAL = 'about;'
-
-  const [modal, setModal] = useState(null)
 
   let favoriteGames = [
     {
       id: 1,
       avatar: user ? user.photo_200 : null,
       title: 'Малиновый джем',
-      description: '3 года 2 месяца 12 минут',
+      description: '1232 часов',
     },
     {
       id: 2,
       avatar: user ? user.photo_200 : null,
       title: 'Малиновый джем 2',
-      description: '8 лет',
+      description: '342 часов',
+    },
+  ]
+
+  let accounts = [
+    {
+      id: 1,
+      avatar: user ? user.photo_200 : null,
+      title: 'Steam',
+      nickname: 'fayfay',
+      since: '20.01.2019',
+    },
+    {
+      id: 2,
+      avatar: user ? user.photo_200 : null,
+      title: 'Battle.nex',
+      nickname: 'megapihar',
+      since: '20.02.2012',
     },
   ]
 
@@ -43,51 +53,75 @@ const Profile = ({ id, user, title }) => {
       <PanelHeader>{title}</PanelHeader>
       {user && (
         <Group>
-          <Cell
+          <RichCell
             before={user.photo_200 ? <Avatar src={user.photo_200} size={72} /> : null}
-            description="Лябудень"
+            caption={<div style={{ display: 'flex', flexDirection: 'column' }}>
+              <Spacing size={4} />
+              <Dropdown
+                content={
+                  <FormLayout>
+                    <FormItem>
+                      <Title level='2'
+                             style={{ textAlign: 'center' }}
+                             weight='medium'>
+                        Статус
+                      </Title>
+                    </FormItem>
+                    <FormItem>
+                      <Input type='text' />
+                    </FormItem>
+                    <FormItem>
+                      <Button size='l' stretched>Сохранить</Button>
+                    </FormItem>
+                  </FormLayout>
+                }
+              >
+                {({ open, setAnchorElement }) => (
+                  <div ref={setAnchorElement}>
+                    <span
+                      style={{ color: '#4986CC' }}
+                      onClick={open}
+                    >
+                      Статус
+                    </span>
+                  </div>
+                )}
+              </Dropdown>
+              <Spacing size={4} />
+              <span>Всего в игре: 5 173 часов</span>
+            </div>}
           >
             {`${user.first_name} ${user.last_name}`}
-          </Cell>
+          </RichCell>
         </Group>)}
-      <MiniInfoCell
-        before={<Icon20ErrorCircleOutline />}
-        mode='add'
-        onClick={() => setModal(ABOUT_MODAL)}
-      >
-        Статус
-      </MiniInfoCell>
-      <Spacing size={40} />
+      <Group>
+        <Header mode='primary'>Аккаунты</Header>
+        {accounts.map(account => {
+            return (
+              <SimpleCell key={account.id} before={<Avatar mode='app' size={32} src={account.avatar} />}
+                          description={<div style={{display: 'flex', alignItems: 'center'}}><Icon12User /> {account.nickname}</div>}
+              >
+
+                {account.title}
+                <span style={{color: '#818C99'}}> с {account.since}</span>
+              </SimpleCell>)
+          },
+        )}
+      </Group>
       <Group>
         <Header mode='primary'>Любимые игры</Header>
         {favoriteGames.map(game => {
             return (
-              <SimpleCell key={game.id} before={<Avatar mode='app' size={72} src={game.avatar} />}
-                          description={game.description}>
+              <SimpleCell key={game.id} before={<Avatar mode='app' size={32} src={game.avatar} />}
+                          description={game.description}
+                          after={<Icon24UnfavoriteOutline />}
+              >
+
                 {game.title}
               </SimpleCell>)
           },
         )}
       </Group>
-      <ModalRoot activeModal={modal} onClose={() => setModal(null)}>
-        <ModalPage
-          id={ABOUT_MODAL}
-          header={
-            <ModalPageHeader>
-              Расскажите о себе
-            </ModalPageHeader>
-          }
-        >
-          <FormLayout>
-            <FormItem>
-              <Input type='text' />
-            </FormItem>
-            <FormItem>
-              <Button size='l' stretched>Сохранить</Button>
-            </FormItem>
-          </FormLayout>
-        </ModalPage>
-      </ModalRoot>
     </Panel>
   )
 }
